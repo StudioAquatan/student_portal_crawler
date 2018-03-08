11a554743282623e428b418c1483ce5bd1d54e1c
pudding <pudding@mail.poyo.info>
2018-03-08 15:31:31 +0900

    refs #5 refs #6 refs #7 add base parser modules

---
+++ kit_portal_crawler/parser/__init__.py
@@ -0,0 +1,5 @@
+from .base import BaseParser, GeneralParser, REGISTERED_PARSERS
+from .static import SupportedPages
+from .utils import norm, get_key_from_url
+
+from .lec_info import LectureInformationParser
