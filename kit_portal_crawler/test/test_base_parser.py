0a985764a65109474c96bbe58375682a5b567292
pudding <pudding@mail.poyo.info>
2018-03-10 14:59:43 +0900

    refs #3 add unittest for `parser.GeneralParser`

--- kit_portal_crawler/test/test_base_parser.py
+++ kit_portal_crawler/test/test_base_parser.py
@@ -59,7 +59,33 @@ class TestBaseParser(TestCase):
 
     @raises(AttributeError)
     def test_invalid_inheritance_not_impl(self):
-        """Invalid inheritance (missing url param) test for `base.BaseParser`"""
+        """Invalid inheritance (not implement `parse`) test for `base.BaseParser`"""
         class ParseNotImplParser(BaseParser):
             URL = 'http://www.example.com/'
 
+
+class TestGeneralParser(TestCase):
+    """Test case for `base.GeneralParser`"""
+
+    def test_cls_variable(self):
+        """Class variable validation test for `base.GeneralParser`"""
+        base = BaseParser('')
+        expect_parser_lib = 'lxml'
+        eq_(base.PARSER_LIB, expect_parser_lib)
+        expect_url = '/'
+        eq_(base.URL, expect_url)
+
+    def test_not_register(self):
+        """Check registration test for `base.GeneralParser`"""
+        for k, v in REGISTERED_PARSERS.items():
+            ok_(v is not BaseParser)
+
+    def test_implemented_parse(self):
+        """Check implementation of `parse` method test for `base.GeneralParser`"""
+        general_parser = GeneralParser('')
+        eq_({'data': ''}, general_parser.parse(general_parser._soup))
+        ok_(hasattr(general_parser, 'data'))
+        ok_(hasattr(general_parser, '_soup'))
+        ok_(hasattr(general_parser, 'soup'))
+        ok_(hasattr(general_parser, 'parse'))
+        ok_(hasattr(general_parser, '_data_cache'))
