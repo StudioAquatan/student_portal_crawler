d178d42e75b83a192d28311cc1088b36786c7bc6
pudding <pudding@mail.poyo.info>
2018-03-12 17:54:41 +0900

    fix version

--- student_portal_crawler/__version__.py
+++ student_portal_crawler/__version__.py
@@ -3,7 +3,7 @@
 __title__ = 'student_portal_crawler'
 __description__ = 'Crawler module for KIT Student Portal'
 __url__ = 'https://www.aquatan.studio/'
-__version__ = '0.1.0a1'
+__version__ = '0.1.0-alpha1'
 __author__ = 'Studio Aquatan'
 __author_email__ = 'studioaquatan@mail.poyo.info'
 __license__ = 'MIT'
